cc840b6f9cb7fbc7ebf080b3bf7191fe76dc8bdc
LWooler <48956441+LWooler@users.noreply.github.com>
2019-11-21 09:21:18 -0600

    window now doesnt need to be visible to work

--- Pokemon_Emerald/shiny_torchic.py
+++ Pokemon_Emerald/shiny_torchic.py
@@ -1,9 +1,12 @@
 import time
 import pyautogui
 from pywinauto.application import Application
-from pywinauto.keyboard import send_keys
 import ctypes
 from ctypes import wintypes
+import win32gui
+import win32ui
+from ctypes import windll
+from PIL import Image
 
 key_press = 258
 key_down = 256
@@ -16,14 +19,6 @@ key_bkspce = 0x08
 ############################################
 ###############Functions####################
 ############################################
-def getColor():
-	PIXEL = pyautogui.screenshot(
-	    region=(
-	        MOUSE_X, MOUSE_Y, 1, 1
-	    )
-	)
-	COLOR = PIXEL.getcolors()
-	return COLOR
 
 def softReset():
 	dlg.send_message(key_down, key_x, 0)
@@ -37,60 +32,110 @@ def softReset():
 	dlg.send_message(key_up, key_bkspce, 0)
 
 def returnToBattle():
-	for x in range(0,90):
+	for x in range(0,75):
 		dlg.send_message(key_down, key_x, 0)
 		time.sleep(.1)
 		dlg.send_message(key_up, key_x, 0)
 		time.sleep(.1)
 	time.sleep(3)
 
+def takePicture(hwnd, int):
+	left, top, right, bot = win32gui.GetWindowRect(hwnd)
+	w = right - left
+	h = bot - top
+
+	hwndDC = win32gui.GetWindowDC(hwnd)
+	mfcDC  = win32ui.CreateDCFromHandle(hwndDC)
+	saveDC = mfcDC.CreateCompatibleDC()
+
+	saveBitMap = win32ui.CreateBitmap()
+	saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+
+	saveDC.SelectObject(saveBitMap)
+
+	# Change the line below depending on whether you want the whole window
+	# or just the client area.
+	#result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 1)
+	result = ctypes.windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 0)
+
+	bmpinfo = saveBitMap.GetInfo()
+	bmpstr = saveBitMap.GetBitmapBits(True)
+
+	im = Image.frombuffer(
+	    'RGB',
+	    (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+	    bmpstr, 'raw', 'BGRX', 0, 1)
+
+	win32gui.DeleteObject(saveBitMap.GetHandle())
+	saveDC.DeleteDC()
+	mfcDC.DeleteDC()
+	win32gui.ReleaseDC(hwnd, hwndDC)
+
+	if result == 1:
+	    #PrintWindow Succeeded
+		if int == 0:
+			im.save('base' + str(hwnd) + '.png')
+		else:
+			im.save("current" + str(hwnd) + ".png")
+
 ############################################
 ###########Main Function####################
 ############################################
 
-time.sleep(.3)
+#SAVE IMMEDIATE DATA, handle and window position
 pyautogui.click()
+handle = win32gui.GetForegroundWindow()
 MOUSE_X, MOUSE_Y = pyautogui.position()
+r = win32gui.GetWindowRect(handle);
+takePicture(handle, 0)
 
 #GET PID
 user32 = ctypes.windll.user32
-h_wnd = user32.GetForegroundWindow()
 pid = wintypes.DWORD()
-user32.GetWindowThreadProcessId(h_wnd, ctypes.byref(pid))
-print(pid.value)
+user32.GetWindowThreadProcessId(handle, ctypes.byref(pid))
 
+#GET WINDOW DIALOG TO SEND KEYSTROKES
 app = Application().connect(process=pid.value)
-#print(app.windows())
 dlg = app.top_window()
 
+#INIT VARIABLES
 start_time = time.time()
-Normal_Color = getColor()
-# print("Mouse: (%d,%d)" % (MOUSE_X, MOUSE_Y))
-#print("RGB: %s" % (Normal_Color[0][1].__str__()))
-
+relative_x = MOUSE_X-r[0]
+relative_y = MOUSE_Y-r[1]
+im1 = Image.open('base' + str(handle) + '.png')
+Normal_Color = im1.getpixel( (relative_x,relative_y) )
+im1.close()
+print("Base color = " + str(Normal_Color))
 found_shiny = False;
 sr_count = 0;
+
 
 while (not found_shiny):
 	softReset()
 	returnToBattle()
-	possible_shiny = getColor()
-	print("RGB: %s" % (possible_shiny[0][1].__str__()) + " SR: " + str(sr_count) + ' Duration ' + time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
-	if possible_shiny[0][1] != Normal_Color[0][1]:
+	takePicture(handle, 1)
+
+	#get color of new screenshot at same pixel as base screenshot
+	im2 = Image.open("current" + str(handle) + ".png")
+	New_Color = im2.getpixel( (relative_x,relative_y) )
+	im2.close()
+
+	print("SR: " + str(sr_count) + ' Duration ' + time.strftime("%d:%H:%M:%S", time.gmtime(time.time() - start_time)))
+	if New_Color != Normal_Color:
 		found_shiny = True;
 	sr_count += 1;
 
 print('Found shiny at ' + str(sr_count) + ' soft resets.')
-print('Duration ' + time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
+print('Duration ' + time.strftime("%d:%H:%M:%S", time.gmtime(time.time() - start_time)))
 
 
 
 
 #INSTRUCTIONS - READ ME
 #Only works on windows 10
-#This is currently only working to soft reset for a shiny torchic on pokemon Emerald
+#This is currently only working to soft reset for a shiny torchic on pokemon ruby/saphire
 #Only known Emulator that this works with is mGBA (way that the keys are passed in)
-#Can be used for as maany instances you can fit on your screen
+#Can be used for as many instances you can fit on your screen
 #Need python32 + pip + all the libraries
 #pip install psutil
 #pip install pyautogui
@@ -98,3 +143,6 @@ print('Duration ' + time.strftime("%H:%M:%S", time.gmtime(time.time() - start_ti
 #
 #1. Go to first encounter with torchic
 #2. Start program while mouse is hovering over torchic to grab color (must do this for each instance)
+#3. It will stop when it finds a shiny (hopefully)
+#
+#Note: You may not close/minimise the emulator, but it doesn't have to be visible
